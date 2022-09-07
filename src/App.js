e7cb141664f75235d0e9ba87c628788ee5f2a8ea
wdparker <wdparker@ncsu.edu>
2022-09-06 22:09:36 -0500

    Initial project creation. Made some fields.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [tableNames, setTableNames] = useState([]);
+  const [fieldNames, setFieldNames] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="test">Hello</h1>
+      <div id="field-selector-wrapper">
+        <form id="selector-array">
+          Enter Data Parameters Below
+          <br />
+          <div id="table-name-entry-wrapper">
+            Table Names:
+            <br />
+            <label for="table-1-name">Table 1 Name: </label>
+            <input
+              type="text"
+              name="table-1-name"
+              id="table-1-name"
+              size="40"
+            ></input>
+            <br />
+            <label for="table-2-name">Table 2 Name: </label>
+            <input
+              type="text"
+              name="table-2-name"
+              id="table-2-name"
+              size="40"
+            ></input>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
