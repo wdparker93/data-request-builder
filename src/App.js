75e5eef222301a5fdf857782b9edfaa306d546f0
wdparker <wdparker@ncsu.edu>
2022-09-08 21:18:47 -0500

    Creation of buttons and basic styling.

--- src/App.js
+++ src/App.js
@@ -5,33 +5,63 @@ function App() {
   const [tableNames, setTableNames] = useState([]);
   const [fieldNames, setFieldNames] = useState([]);
 
+  const addTableName = () => {
+    console.log("added table name");
+  };
+
+  const addFieldName = () => {
+    console.log("added field name");
+  };
+
   return (
     <div className="App">
-      <h1 id="test">Hello</h1>
-      <div id="field-selector-wrapper">
-        <form id="selector-array">
-          Enter Data Parameters Below
+      <h1 id="main-heading">Data Definition Builder</h1>
+      <div id="interactive-section-wrapper">
+        <div id="field-name-wrapper">
+          <h2 id="field-entry-heading">Enter Data Parameter Names</h2>
+          <form id="field-name-array">
+            <div class="table-name-section" id="table-name-entry-wrapper">
+              <br />
+              <label for="table-1-name">Table 1 Name: </label>
+              <input
+                type="text"
+                name="table-1-name"
+                id="table-1-name"
+                size="30"
+              ></input>
+              <div class="field-name-section" id="table-field-entry-wrapper">
+                <br />
+                <label for="table-1-field-1-name">Table 1 Field 1 Name: </label>
+                <input
+                  type="text"
+                  name="table-1-field-1-name"
+                  id="table-1-field-1-name"
+                  size="30"
+                ></input>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div id="button-section-wrapper">
+          <h2 id="button-heading">Add Or Remove Fields</h2>
           <br />
-          <div id="table-name-entry-wrapper">
-            Table Names:
-            <br />
-            <label for="table-1-name">Table 1 Name: </label>
-            <input
-              type="text"
-              name="table-1-name"
-              id="table-1-name"
-              size="40"
-            ></input>
-            <br />
-            <label for="table-2-name">Table 2 Name: </label>
-            <input
-              type="text"
-              name="table-2-name"
-              id="table-2-name"
-              size="40"
-            ></input>
+          <div id="button-wrapper">
+            <button
+              className="table-button"
+              id="add-table-name-button"
+              onClick={addTableName}
+            >
+              Add New Table
+            </button>
+            <button
+              className="field-button"
+              id="add-field-name-button"
+              onClick={addFieldName}
+            >
+              Add New Field
+            </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
