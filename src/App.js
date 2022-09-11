8ffc1af1bbc67400bc2cdea96bb58b71cbaa1f44
wdparker <wdparker@ncsu.edu>
2022-09-11 18:24:34 -0500

    Data fields now rendering with correct heirarchy.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import EntryFields from "./secondary-components/js/EntryFields.js";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [tableNames, setTableNames] = useState([]);
-  const [fieldNames, setFieldNames] = useState([]);
+  const [tableNamesDict, setTableNamesDict] = useState({ 1: "T1", 2: "T2" });
+  const [fieldNamesDict, setFieldNamesDict] = useState({
+    1: ["T1F1", "T1F2"],
+    2: ["T2F1", "T2F2"],
+  });
   const [numTables, setNumTables] = useState(1);
   const [numFields, setNumFields] = useState(1);
 
@@ -19,12 +22,45 @@ function App() {
   };
 
   const submitFieldData = () => {
-    console.log(tableNames);
-    console.log(fieldNames);
+    //console.log(tableNames);
+    //console.log(fieldNames);
     console.log(numTables);
     console.log(numFields);
   };
 
+  const captureState = () => {
+    let inputs = document.getElementsByClassName("input-field");
+    let tableNamesWorkingDict = {};
+    let fieldNamesWorkingDict = {};
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      let inputName = input.name;
+      let inputId = input.id;
+      //inputName will either be like table-1-name or table-1-field-1-name
+      if (!inputName.includes("field")) {
+        let tableNumber = parseInt(inputName.charAt(6));
+        tableNamesWorkingDict[tableNumber] =
+          document.getElementById(inputId).value;
+      } else {
+        let tableNumber = parseInt(inputName.charAt(6));
+        let addAtFieldIndex = parseInt(inputName.charAt(14)) - 1;
+        let currentTableFieldNames = [];
+        if (typeof fieldNamesWorkingDict[tableNumber] !== "undefined") {
+          currentTableFieldNames = fieldNamesWorkingDict[tableNumber];
+        }
+        currentTableFieldNames.splice(
+          addAtFieldIndex,
+          0,
+          document.getElementById(inputId).value
+        );
+        fieldNamesWorkingDict[tableNumber] = currentTableFieldNames;
+      }
+    }
+    setTableNamesDict(tableNamesWorkingDict);
+    setFieldNamesDict(fieldNamesWorkingDict);
+  };
+
+  /*
   const handleNameChange = () => {
     let inputs = document.getElementsByClassName("input-field");
     let tableNamesWorkingArr = [];
@@ -61,6 +97,7 @@ function App() {
     //console.log(tableNamesWorkingArr);
     //console.log(fieldNamesWorkingArr);
   };
+  */
 
   return (
     <div className="App">
@@ -69,15 +106,14 @@ function App() {
         <div id="field-name-wrapper">
           <h2 id="field-entry-heading">Enter Data Parameter Names</h2>
           <EntryFields
-            nameChangeHandler={handleNameChange}
-            tableNamesParam={tableNames}
-            fieldNamesParam={fieldNames}
+            tableNamesParam={tableNamesDict}
+            fieldNamesParam={fieldNamesDict}
             numTablesParam={numTables}
             numFieldsParam={numFields}
           />
         </div>
         <div id="button-section-wrapper">
-          <h2 id="button-heading">Add Or Remove Fields</h2>
+          <h2 id="button-heading">Add / Remove / Submit Fields</h2>
           <br />
           <div id="button-wrapper">
             <button
@@ -99,7 +135,14 @@ function App() {
               id="submit-field-data-button"
               onClick={submitFieldData}
             >
-              Submit Fields
+              Export Data
+            </button>
+            <button
+              className="field-button"
+              id="test-capture-state-button"
+              onClick={captureState}
+            >
+              Save Data
             </button>
           </div>
         </div>
--- src/secondary-components/js/EntryFields.js
+++ src/secondary-components/js/EntryFields.js
@@ -1,40 +1,86 @@
 import "../css/EntryFields.css";
 
 function EntryFields(props) {
-  console.log(props.nameChangeHandler);
-  console.log(props.tableNamesParam);
-  console.log(props.fieldNamesParam);
-  console.log(props.numTablesParam);
-  console.log(props.numFieldsParam);
+  //Produces an element that will contain a field or table name
+  const DataElement = (tableNameElementData) => {
+    let data = tableNameElementData.elementData;
+    //Fields will be logically set based on whether its a field or table
+    let tableNumber = data.charAt(1);
+    let fieldNumber = "";
+    let elementValue = "";
+    let htmlElementName = "table-";
+    let htmlLabelValue = "Table ";
+    let htmlClassName = "";
+    //The entire array value including labeler (T1F1, etc.)
+    let indexOfColon = data.indexOf(":");
+    if (indexOfColon === 2) {
+      //A table element
+      elementValue = data.substring(3);
+      htmlElementName += tableNumber + "-name";
+      htmlLabelValue += tableNumber;
+      htmlClassName = "table-element";
+    } else if (indexOfColon === 4) {
+      //A field element
+      fieldNumber = data.charAt(3);
+      elementValue = data.substring(5);
+      htmlElementName += tableNumber + "field-" + fieldNumber + "-name";
+      htmlLabelValue += tableNumber + " Field " + fieldNumber;
+      htmlClassName = "field-element";
+    }
+    return (
+      <>
+        <form className={htmlClassName}>
+          <label htmlFor={htmlElementName}>{htmlLabelValue}: </label>
+          <input
+            type="text"
+            name={htmlElementName}
+            id={htmlElementName}
+            size="30"
+            className="input-field"
+            defaultValue={elementValue}
+          ></input>
+        </form>
+      </>
+    );
+  };
+
+  const convertDictToArray = (tableOrField) => {
+    let returnArray = [];
+    let counter = 0;
+    let dictToConvert = {};
+    if (tableOrField === "T") {
+      dictToConvert = props.tableNamesParam;
+      for (const [key, value] of Object.entries(dictToConvert)) {
+        let arrayValue = "T" + key + ":" + value;
+        returnArray.splice(counter++, 0, arrayValue);
+      }
+    } else if (tableOrField === "F") {
+      dictToConvert = props.fieldNamesParam;
+      for (const [key] of Object.entries(dictToConvert)) {
+        let fieldCounter = 1;
+        for (const [value] of Object.entries(dictToConvert[key])) {
+          let arrayValue =
+            "T" + key + "F" + fieldCounter++ + ":" + dictToConvert[key][value];
+          returnArray.splice(counter++, 0, arrayValue);
+        }
+      }
+    }
+    return returnArray;
+  };
+
+  //Convert the dictionaries to arrays, combine them, and sort the entries.
+  let tableNameArray = convertDictToArray("T");
+  let fieldNameArray = convertDictToArray("F");
+  let aggregateArray = tableNameArray.concat(fieldNameArray);
+  aggregateArray = aggregateArray.sort();
+
+  const renderOutputDataElements = () => {
+    return aggregateArray.map((el) => <DataElement elementData={el} />);
+  };
 
   return (
     <>
-      <form id="field-name-array">
-        <div className="table-name-section">
-          <br />
-          <label htmlFor="table-1-name">Table 1 Name: </label>
-          <input
-            type="text"
-            name="table-1-name"
-            id="table-1-name"
-            size="30"
-            className="input-field"
-            onChange={props.nameChangeHandler}
-          ></input>
-          <div className="field-name-section">
-            <br />
-            <label htmlFor="table-1-field-1-name">Table 1 Field 1 Name: </label>
-            <input
-              type="text"
-              name="table-1-field-1-name"
-              id="table-1-field-1-name"
-              size="30"
-              className="input-field"
-              onChange={props.nameChangeHandler}
-            ></input>
-          </div>
-        </div>
-      </form>
+      <div id="output-html-wrapper">{renderOutputDataElements()}</div>
     </>
   );
 }
