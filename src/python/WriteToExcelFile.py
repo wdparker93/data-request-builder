37e2e445e16b4eaf1955255f2a351f6e1943cb14
wdparker <wdparker@ncsu.edu>
2022-09-18 21:58:13 -0500

    Import openpyxl. Dummy test python script.

---
+++ src/python/WriteToExcelFile.py
@@ -0,0 +1,16 @@
+import openpyxl
+
+outputPath = 'C:\\Users\\wdpar\\vs_code_repos\\data-request-builder\\docs\\output\\OutputDataDefinition.xlsx'
+
+outputDataDefWB = openpyxl.load_workbook(outputPath)
+tableSheet = outputDataDefWB.active
+
+tableNameEntryCol = 1
+outputRow = 1
+
+nameArray = ['Will', 'is', 'cool']
+
+with(open(outputPath, "a")) as f:
+    for i in range(len(nameArray)):
+        tableSheet.cell(row=outputRow, column=tableNameEntryCol).value = nameArray[i]
+        outputRow += 1
